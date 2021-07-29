07a1a9421c698e33cc15fa3c2c60b399e0f0a8a8
Nathan <denoyelles.nathan@gmail.com>
2021-07-28 19:25:32 -0500

    Add missing implementation of BasicService to DatasetService.

--- src/main/java/com/api/challenge/service/DatasetService.java
+++ src/main/java/com/api/challenge/service/DatasetService.java
@@ -2,18 +2,11 @@ package com.api.challenge.service;
 
 import com.api.challenge.model.Dataset;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
-public class DatasetService {
-
-    final RestTemplate restTemplate;
-
-    public DatasetService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+public class DatasetService extends BasicService {
 
     public Dataset getDataset() {
-        return restTemplate.getForObject("http://api.coxauto-interview.com/api/datasetId", Dataset.class);
+        return get("http://api.coxauto-interview.com/api/datasetId", Dataset.class);
     }
 }
